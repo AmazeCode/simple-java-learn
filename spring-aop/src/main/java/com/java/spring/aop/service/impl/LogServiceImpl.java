37b140fe0485913513a9ae3976bb9f647b76a905
zhangyadong <1558628010@qq.com>
2020-12-21 15:11:36 +0800

    自定义事物、事物传播行为

--- spring-aop/src/main/java/com/java/spring/aop/service/impl/LogServiceImpl.java
+++ spring-aop/src/main/java/com/java/spring/aop/service/impl/LogServiceImpl.java
@@ -24,10 +24,10 @@ public class LogServiceImpl implements LogService {
         事物的传播行为(7种)：(所说的当前事物都是指的外层service方法的事物)
         PROPAGATION_REQUIRED:如果当前有事物，就用当前事物，如果当前没事物，就新建一个事物。(这个是最常见的)
         PROPAGATION_SUPPORTS:支持当钱事物，如果当前没有事物，就以非事物方式进行。//如果外层方法没有事物，内层被调用的service方法就以非事物进行执行
-        PROPAGATION_MANDATORY:支持当前事物，如果当前没有事物，就抛出异常
-        PROPAGATION_REQUIRES_NEW:新建事物，如果当前事物存在，就把当前事物挂起
+        PROPAGATION_MANDATORY:支持当前事物，如果当前没有事物，就抛出异常   //支持外层事物，如果外层没有事物，就抛出异常
+        PROPAGATION_REQUIRES_NEW:新建事物，如果当前事物存在，就把当前事物挂起  //内层新建事物，如果外层事物存在就把外层事物挂起
         PROPAGATION_NOT_SUPPORTED：以非事物方式执行操作，如果当前事物存在，就把当前事物挂起//内层service方法以非事物执行，如果外层事物存在就把当前事物挂起
-        PROPAGATION_NEVER:以非事物方式执行，如果当前存在事物，则抛出异常
+        PROPAGATION_NEVER:以非事物方式执行，如果当前存在事物，则抛出异常 //内层service方法以非事物执行，如果外层事物存在就抛出异常
         PROPAGATION_NESTED：如果一个活动的事务存在，则运行在一个嵌套的事务中。 如果没有活动事务, 则按TransactionDefinition.PROPAGATION_REQUIRED 属性执行
         注意：PROPAGATION_NESTED 与PROPAGATION_REQUIRES_NEW的区别:
         它们非常类似,都像一个嵌套事务，如果不存在一个活动的事务，都会开启一个新的事务。
