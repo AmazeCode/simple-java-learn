3550161f8661a42afb98648a4e036fdadb5efb90
zhangyadong <1558628010@qq.com>
2021-01-02 23:24:18 +0800

    手写集合框架项目创建

---
+++ collection/src/main/java/com/java/collection/arraylist/Test.java
@@ -0,0 +1,15 @@
+package com.java.collection.arraylist;
+
+/**
+ * @Description: 手些ArrayList
+ * @Author: zhangyadong
+ * @Date: 2021/1/2 23:20
+ * @Version: v1.0
+ */
+public class Test {
+    
+    /*
+        1. 集合底层使用数组实现
+        2. 怎么保证数组中存放无限大小 #### 数组扩容技术
+     */
+}
