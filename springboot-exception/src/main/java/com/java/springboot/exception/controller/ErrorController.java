f5779054f700b3abb10359efdde60e4a55dcf7fb
zhangyadong <1558628010@qq.com>
2021-01-14 18:04:02 +0800

    SpringBoot全局异常处理

--- springboot-exception/src/main/java/com/java/springboot/exception/controller/ErrorController.java
+++ springboot-exception/src/main/java/com/java/springboot/exception/controller/ErrorController.java
@@ -3,6 +3,7 @@ package com.java.springboot.exception.controller;
 import com.java.springboot.exception.exception.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,7 @@ public class ErrorController {
     @ResponseBody
     @RequestMapping("/getUser")
     public String getUser() {
+        //实际开发中怎么把错误信息记录到日志中?
         throw new NullPointerException(); //模拟json接口访问异常
     }
 
@@ -49,7 +51,7 @@ public class ErrorController {
 
 
     /**
-     * @description: thymeleaf页面传值(方式一)
+     * @description: thymeleaf页面传值第一种方式
      * @params: []
      * @return: java.lang.String
      * @author: zhangyadong
@@ -59,17 +61,17 @@ public class ErrorController {
     public ModelAndView dataThymeleafOne() {
         ModelAndView modelAndView = new ModelAndView();
         try {
-            modelAndView.addObject("msg","我是传递过来的值。。。。。【第一种写法】");
+            modelAndView.addObject("msg","我是传递过来的值。。。。。【thymeleaf页面传值第一种方式】");
             int i = 1/0; //模拟页面访问出错异常
             modelAndView.setViewName("/page/dataThymeleaf");
             return modelAndView;
         } catch (RuntimeException e) {
-            throw new MyException("500","系统异常--第一种访问页面方式");
+            throw new MyException("500","系统异常--thymeleaf页面传值第一种方式");
         }
     }
 
     /**
-     * @description: thymeleaf页面传值(方式二)
+     * @description: thymeleaf页面传值第二种方式
      * @params: [model]
      * @return: java.lang.String
      * @author: zhangyadong
@@ -81,11 +83,33 @@ public class ErrorController {
             注意不能直接在方法上抛异常,否则不会走异常处理,而是需要在方法内使用try-catch后再抛出指定的异常,才能正常走异常处理
          */
         try {
-            model.addAttribute("msg","我是传递过来的值。。。。。【第二种写法】");
+            model.addAttribute("msg","我是传递过来的值。。。。。【thymeleaf页面传值第二种方式】");
             int i = 1/0;
             return "/page/dataThymeleaf";
         } catch (RuntimeException e){
-            throw new MyException("500","系统异常--第二种访问页面方式");
+            throw new MyException("500","系统异常--thymeleaf页面传值第二种方式");
+        }
+    }
+
+
+    /**
+     * @description: thymeleaf第三种向页面传值的方式
+     * @params: [modelMap]
+     * @return: java.lang.String
+     * @author: zhangyadong
+     * @date: 2021/1/14 17:54
+     */
+    @RequestMapping("/dataThymeleafThree")
+    public String dataThymeleafThree(ModelMap modelMap) {
+        /*
+            注意不能直接在方法上抛异常,否则不会走异常处理,而是需要在方法内使用try-catch后再抛出指定的异常,才能正常走异常处理
+         */
+        try {
+            modelMap.put("msg","我是传递过来的值。。。。。【thymeleaf页面传值第三种方式】");
+            int i = 1/0;
+            return "/page/dataThymeleaf";
+        } catch (RuntimeException e){
+            throw new MyException("500","系统异常--thymeleaf页面传值第三种方式");
         }
     }
 }
