a3368a8f8f440450ba204454ce288376f82d8b53
zhangyadong <1558628010@qq.com>
2021-04-20 14:17:15 +0800

    锁-安全

---
+++ work-common-error/src/main/java/com/ac/common/error/code/controller/lock/LockLevelController.java
@@ -0,0 +1,44 @@
+package com.ac.common.error.code.controller.lock;
+
+import com.ac.common.error.code.controller.lock.pojo.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.IntStream;
+
+/**
+ * @Description: 锁测试
+ * @Author: zhangyadong
+ * @Date: 2021/4/20 11:03
+ * @Version: v1.0
+ */
+@RestController
+@RequestMapping("locklevelcontroller")
+public class LockLevelController {
+
+    /*
+        加锁前先要清楚锁和对象是不是一个层面的：
+            静态字段属于类,类级别的锁才能保护;而非静态字段属于实例,实例级别的锁就可以保护
+     */
+    // 反例
+    @GetMapping("wrong")
+    public int wrong (@RequestParam(value = "count",defaultValue = "1000000") int count) {
+
+        Data.reset();
+        //多线程循环一定次数调用Data类不同实列的wrong方法
+        IntStream.rangeClosed(1, count).parallel().forEach(i -> new Data().wrong());
+        return Data.getCounter();
+    }
+
+    // 正例
+    @GetMapping("right")
+    public int right (@RequestParam(value = "count",defaultValue = "1000000") int count) {
+
+        Data.reset();
+        //多线程循环一定次数调用Data类不同实列的wrong方法
+        IntStream.rangeClosed(1, count).parallel().forEach(i -> new Data().right());
+        return Data.getCounter();
+    }
+}
---
+++ work-common-error/src/main/java/com/ac/common/error/code/controller/lock/pojo/Data.java
@@ -0,0 +1,39 @@
+package com.ac.common.error.code.controller.lock.pojo;
+
+import lombok.Getter;
+
+/**
+ * @Description:
+ * @Author: zhangyadong
+ * @Date: 2021/4/20 10:54
+ * @Version: v1.0
+ */
+public class Data {
+
+    // 静态counter属于类,在多个实例中共享
+    @Getter
+    private static int counter = 0;
+
+    public static int reset() {
+        counter = 0;
+        return counter;
+    }
+
+    /*
+        另外一种修改方法是wrong直接写成静态,但我们不可能为了解决线程安全问题改变代码结构，把实例方法改为静态方法。
+     */
+    public synchronized void wrong() {
+        counter++;
+    }
+
+    /*
+        修改方式：同样定义一个Object类型的静态字段,在操作counter之前对这个字段加锁
+     */
+    private static Object locker = new Object();
+
+    public void right() {
+        synchronized (locker) {
+            counter++;
+        }
+    }
+}
