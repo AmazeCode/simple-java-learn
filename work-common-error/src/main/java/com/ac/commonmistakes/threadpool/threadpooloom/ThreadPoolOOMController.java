c499df3b7864961b9a24cf6c390b6ccb96fba3f6
zhangyadong <1558628010@qq.com>
2021-05-07 17:47:47 +0800

    业务开发常见错误100例 线程池溢出示例

---
+++ work-common-error/src/main/java/com/ac/commonmistakes/threadpool/threadpooloom/ThreadPoolOOMController.java
@@ -0,0 +1,18 @@
+package com.ac.commonmistakes.threadpool.threadpooloom;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 线程池溢出 【线程池线程管理策略详解、线程池的声明需要手动进行】
+ * @Author: zhangyadong
+ * @Date: 2021/5/7 17:45
+ * @Version: v1.0
+ */
+@Slf4j
+@RequestMapping("threadpooloom")
+@RestController
+public class ThreadPoolOOMController {
+
+}
